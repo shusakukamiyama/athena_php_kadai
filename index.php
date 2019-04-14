9e6180501fd4ab4937be8b60250e2f1bd450fff5
shusakukamiyama <xyz000shupi000@gmail.com>
2019-04-14 14:23:29 +0900

    achive making simple board

--- index.php
+++ index.php
@@ -2,17 +2,57 @@
 
 $dataFile = 'bbs.dat';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+//CSRF対策
 
-    $message = $_POST['message'];
-    $user = $_POST['user'];
+session_start();
 
-    $newData = $message . "\t" . $user . "\n";
-
-    $fp = fopen($dataFile, 'a');
-    fwrite($fp, $newData);
-    fclose($fp);
+function setToken() {
+    $token = sha1(uniqid(mt_rand(), true));
+    $_SESSION['token'] = $token;
 }
+
+function checkToken() {
+    if (empty($_SESSION['token']) || ($_SESSION['token'] != $_POST['token'] )) {
+        echo "不正なPOSTが行われました！";
+        exit;
+    }
+}
+
+function h($s) {
+    return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && 
+    isset($_POST['message']) &&
+    isset($_POST['user'])) {
+
+    checkToken();
+
+    $message = trim($_POST['message']);
+    $user = trim($_POST['user']);
+    
+    if ($message !== '') {
+
+        $user = ($user === '') ? '名無し' :$user;
+
+        $message = str_replace("\t", ' ', $message);
+        $user= str_replace("\t", ' ', $user);
+
+        $postedAt = date('Y-m-d H:i:s');
+
+        $newData = $message . "\t" . $user . "\t" . $postedAt . "\n";
+
+        $fp = fopen($dataFile, 'a');
+        fwrite($fp, $newData);
+        fclose($fp);
+    }
+} else {
+    setToken();
+}
+
+$posts = file($dataFile, FILE_IGNORE_NEW_LINES);
+
+$posts = array_reverse($posts);
 
 ?>
 
@@ -30,10 +70,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         message: <input type="text" name="message">
         user: <input type="text" name="user">
         <input type="submit" value="投稿">
+        <input type="hidden" name="token" value="<?php echo h($_SESSION['token']); ?>">
     </form>
-    <h2>投稿一覧（０件）</h2>
+    <h2>投稿一覧（<?php echo count($posts); ?>件）</h2>
     <ul>
-        <li>まだ投稿はありません。</li>
+        <?php if (count($posts)): ?>
+            <?php foreach($posts as $post): ?>
+                <?php list($message, $user, $postedAt) = explode("\t", $post); ?>
+                <li><?php echo h($message) ?> (<?php echo h($user) ?>) - <?php echo h($postedAt) ?></li>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <li>まだ投稿はありません。</li>
+        <?php endif; ?>
     </ul>
 </body>
 </html>
