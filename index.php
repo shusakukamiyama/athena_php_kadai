10913c960b1c9e182809a13829280fe6d0c97735
shusakukamiyama <xyz000shupi000@gmail.com>
2019-04-14 13:15:21 +0900

    make index.html

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>簡易掲示板</title>
+</head>
+<body>
+    <h1>簡易掲示板</h1>
+    <form>
+        message: <input type="text" name="message">
+        user: <input type="text" name="user">
+    </form>
+    <h2>投稿一覧（０件）</h2>
+    <ul>
+        <li>まだ投稿はありません。</li>
+    </ul>
+</body>
+</html>
